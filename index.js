25a135dc52beac321937656b79f0c37a79659ef3
snatera15 <snatera09@gmail.com>
2015-08-03 23:52:19 -0400

    added stuff

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var express 	   = require('express');
-var app			   = express();
+var express = require('express');
+var app = express();
 
 var port = process.env.PORT || 9000; 
+
+app.use(express.static(__dirname + '/app'));
 
 app.listen(port);      							  // startup our app at http://localhost:8080         
 
