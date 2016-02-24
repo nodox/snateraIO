1778af05c5fc3a101a87874bb4d282c697863afa
nodox <snatera09@gmail.com>
2016-02-24 15:09:49 -0600

    fix js files to be eslint friendly

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,22 +1,32 @@
-// jshint devel:true
-$.material.init();
+/* global $:true */
+'use strict';
 
 
-// smooth scrolling 
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
+$(document).ready(function () {
+  //your code here
+
+
+  // jshint devel:true
+  $.material.init();
+
+
+  // smooth scrolling
+  $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
       }
-    }
+    });
   });
+
+
 });
+
 
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var port = process.env.PORT || 9000;
 
 app.use(express.static(__dirname + '/app'));
 
-app.listen(port);      							  // startup our app at http://localhost:8080         
+app.listen(port);
 
 // shoutout to the user                     
 console.log('Magic happens on port ' + port);
